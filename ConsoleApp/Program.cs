e218d36fdf9d7badee289abf765ede2fc1aa36fc
David07994415 <david.game.power@gmail.com>
2023-12-20 09:53:34 +0800

    task 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("new");
         }
     }
 }
